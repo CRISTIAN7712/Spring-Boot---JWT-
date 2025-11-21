e0afe03d5e4212e9e13bbd66dc0dd8cb18a4d0f6
cristian7712 <cristian.diaz8918@gmail.com>
2025-11-21 12:45:27 -0500

    First Commit

---
+++ src/main/java/com/programandoenjava/jwt/auth/repository/TokenRepository.java
@@ -0,0 +1,20 @@
+package com.programandoenjava.jwt.auth.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TokenRepository extends JpaRepository<Token, Integer> {
+
+    // CAMBIO AQUÍ: Usar t.isExpired y t.isRevoked
+    @Query(value = """
+      select t from Token t inner join User u\s
+      on t.user.id = u.id\s
+      where u.id = :id and (t.isExpired = false or t.isRevoked = false)\s
+      """)
+    List<Token> findAllValidTokenByUser(Integer id);
+
+    Optional<Token> findByToken(String token);
+}
